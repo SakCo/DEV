3c37dc2a90fa9337f30041e1d791b871f75cd6a3
SakCo <sakshi.s.singh2003@gmail.com>
2024-05-06 15:35:13 +0530

    Initial commit

---
+++ CSS/IdeaProjects/hello world/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("i am sakshi");
+        System.out.println("tourist,explorar,student");
+        System.out.println("friendly in nature and love to explore the world");
+        System.out.println("lets explore our culture of our beautiful country INDIA");
+        System.out.println("INDIA is my pride");
+
+    }
+}
